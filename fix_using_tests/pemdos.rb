f0ab61c7e0f014d68910c9bd37b3e9495931a427
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-21 00:43:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,7 +2,8 @@
 require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    additional = 10.times do puts 's' end
+    def additional 
+      10.times do puts 's' end
      "#{additional}string"
   else
     string
