5a05a54fe80b63a12cb6c342ec977f3784c0ce93
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-21 01:18:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,9 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def snake_it_up(string)
-  x= 10.times {puts "s"}
+  x= (10.times {puts "s"}
   if string[0] == "s" 
-     x"#{string}"
+     "#{x}#{string}"
   else
    string
 end
